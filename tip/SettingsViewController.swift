ef369311c87cf3d48e9994257631f24036627afc
Matthew Mogannam <matthewmogannam@icloud.com>
2020-08-15 15:43:03 -0700

    Added all of the functionality of the app.

---
+++ tip/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  tip
+//
+//  Created by Matthew Mogannam on 8/15/20.
+//  Copyright © 2020 Matthew Mogannam. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
